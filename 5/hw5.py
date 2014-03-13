a0b76532e245727c87cf454c6bed6d1040d4d5ed
Snuggert <abe.wiersma@hotmail.nl>
2014-03-13 15:16:26 +0100

    ja

--- 5/hw5.py
+++ 5/hw5.py
@@ -1,13 +1,16 @@
+from random import randrange
+
+
 def main():
     p = 2000000011
     q = 2000000033
     n = p * q
 
-    divisor egcd(randrange(1, n), p - 1 * q - 1)
-    while()
-
-    divisor, x, y = egcd(p, q)
-    print divisor, x, y
+    e = randrange(1, n)
+    divisor, x, y = egcd(e, p - 1 * q - 1)
+    while(divisor != 1):
+        e = randrange(1, n)
+        divisor, x, y = egcd(e, p - 1 * q - 1)
 
 
 def egcd(a, b):
