41126d4d031751a3dd2c0d1a4a94ea1629b98828
Jelte Fennema <jelte12345@gmail.com>
2014-03-13 17:00:35 +0100

    Finnish 5.1

--- 5/hw5.py
+++ 5/hw5.py
@@ -1,16 +1,33 @@
+#!/usr/bin/python3
+
 from random import randrange
 
 
 def main():
-    p = 2000000011
-    q = 2000000033
+    p = 101
+    q = 251
     n = p * q
+    phi_n = (p - 1) * (q - 1)
+    e = 0
+    d = 0
+    m = 25
 
-    e = randrange(1, n)
-    divisor, x, y = egcd(e, p - 1 * q - 1)
-    while(divisor != 1):
-        e = randrange(1, n)
-        divisor, x, y = egcd(e, p - 1 * q - 1)
+    while True:
+        e = randrange(2, phi_n)
+        divisor, x, y = egcd(e, phi_n)
+
+        if divisor == 1:
+            d = modinv(e, phi_n)
+            if d is not False:
+                break
+
+    print('message: ' + str(m))
+    print('encoding key: ' + str(e))
+    print('decoding key: ' + str(d))
+    c = m**e % n
+    print('encoded message: ' + str(c))
+    dec = c**d % n
+    print('decoded message: ' + str(dec))
 
 
 def egcd(a, b):
@@ -19,6 +36,14 @@ def egcd(a, b):
     else:
         g, y, x = egcd(b % a, a)
         return g, x - (b // a) * y, y
+
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        return False
+    else:
+        return x % m
 
 if __name__ == '__main__':
     main()
