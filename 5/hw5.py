ccf39746e49b8992807d046277796aff2fd762f5
Snuggert <abe.wiersma@hotmail.nl>
2014-03-13 15:13:13 +0100

    work work

--- 5/hw5.py
+++ 5/hw5.py
@@ -1,6 +1,21 @@
 def main():
-    prime1 = 2000000011
-    prime2 = 2000000033
+    p = 2000000011
+    q = 2000000033
+    n = p * q
+
+    divisor egcd(randrange(1, n), p - 1 * q - 1)
+    while()
+
+    divisor, x, y = egcd(p, q)
+    print divisor, x, y
+
+
+def egcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        g, y, x = egcd(b % a, a)
+        return g, x - (b // a) * y, y
 
 if __name__ == '__main__':
     main()
